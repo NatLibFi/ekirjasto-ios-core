21068e7ff527d6977f7feaa0481cd6e64ae5a126
Max Gräsbeck <max.grasbeck@helsinki.fi>
2024-03-18 13:16:46 +0200

    Add links in the simplest way possible
    This is the simplest way to add the links and will probably be changed
    in the near future when we figure out how to do so.
    
    Refs EKIR-44, SIMPLYE-234

--- Palace/Settings/TPPSettings.swift
+++ Palace/Settings/TPPSettings.swift
@@ -19,9 +19,9 @@ import Foundation
     return TPPSettings.shared
   }
 
-  static let TPPAboutPalaceURLString = "http://thepalaceproject.org/"
-  static let TPPUserAgreementURLString = "https://legal.palaceproject.io/End%20User%20License%20Agreement.html"
-  static let TPPPrivacyPolicyURLString = "https://legal.palaceproject.io/Privacy%20Policy.html"
+  static let TPPAboutPalaceURLString = "https://lib.e-kirjasto.fi/palaute"
+  static let TPPUserAgreementURLString = "https://www.kansalliskirjasto.fi/fi/e-kirjasto/e-kirjaston-saavutettavuusseloste"
+  static let TPPPrivacyPolicyURLString = "https://www.kansalliskirjasto.fi/fi/e-kirjasto/e-kirjaston-tietosuoja-ja-rekisteriseloste"
   
   static private let customMainFeedURLKey = "NYPLSettingsCustomMainFeedURL"
   static private let accountMainFeedURLKey = "NYPLSettingsAccountMainFeedURL"
