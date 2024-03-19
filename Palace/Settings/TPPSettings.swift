be9218a84d11557ad5e731ff6abbf5115c07ec76
Max Gräsbeck <max.grasbeck@helsinki.fi>
2024-03-19 16:04:11 +0200

    Add settings page buttons
    Adds the buttons to the settings page in the recommended way.
    
    Refs EKIR-44, SIMPLYE-234

--- Palace/Settings/TPPSettings.swift
+++ Palace/Settings/TPPSettings.swift
@@ -19,10 +19,12 @@ import Foundation
     return TPPSettings.shared
   }
 
-  static let TPPAboutPalaceURLString = "https://lib.e-kirjasto.fi/palaute"
+  static let TPPAboutPalaceURLString = "http://thepalaceproject.org/"
   static let TPPUserAgreementURLString = "https://www.kansalliskirjasto.fi/fi/e-kirjasto/e-kirjaston-saavutettavuusseloste"
   static let TPPPrivacyPolicyURLString = "https://www.kansalliskirjasto.fi/fi/e-kirjasto/e-kirjaston-tietosuoja-ja-rekisteriseloste"
-  
+  static let TPPFeedbackURLString = "https://lib.e-kirjasto.fi/palaute"
+  static let TPPAccessibilityURLString = "https://www.kansalliskirjasto.fi/fi/e-kirjasto/e-kirjaston-saavutettavuusseloste"
+
   static private let customMainFeedURLKey = "NYPLSettingsCustomMainFeedURL"
   static private let accountMainFeedURLKey = "NYPLSettingsAccountMainFeedURL"
   static private let userPresentedAgeCheckKey = "NYPLUserPresentedAgeCheckKey"
