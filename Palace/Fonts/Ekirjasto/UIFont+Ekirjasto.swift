e95f21bf41c98ddb36107b89db2f47e94e5d1ab9
Kaisa Kuivalahti <kaisa.kuivalahti@helsinki.fi>
2024-08-30 15:58:14 +0300

    EKIR-325 Scales given fonts to given scale size

--- Palace/Fonts/Ekirjasto/UIFont+Ekirjasto.swift
+++ Palace/Fonts/Ekirjasto/UIFont+Ekirjasto.swift
@@ -11,7 +11,7 @@ import Foundation
 extension UIFont {
 
     /**
-     The below three functions return a dynamically scaled custom font for the specified text style and weight.
+     The below three functions return a dynamically scaled custom font for the specified text style and weight. If the user has changed the font size in the app, it won't allow system font size changes, and vice versa.
 
      - Parameters:
        - textStyle: The text style to use for the font (e.g., `.body`, `.headline`).
@@ -20,22 +20,88 @@ extension UIFont {
      */
 
   @objc class func palaceFont(for textStyle: UIFont.TextStyle, weight: UIFont.Weight = .regular) -> UIFont {
+
+    // Retrieve the user-selected font size adjustment (if any)
+    let fontSizeAdjusted = UserDefaults.standard.bool(forKey: "enablePreferences")
+    
+    // Retrieve the multiplier to be applied to given font
+    var fontMultiplier = UserDefaults.standard.double(forKey: "fontMultiplier")
+    if fontMultiplier == 0.0 {
+      fontMultiplier = 1.0
+    }
+    
+    if fontSizeAdjusted {
+      // Apply user font size adjustment to the given font size
+      let baseFontSize = UIFont.preferredFont(forTextStyle: textStyle).pointSize
+      let setFontSize: CGFloat
+      setFontSize = baseFontSize * fontMultiplier
+      print("Adjusted size: \(setFontSize), multiplier: \(fontMultiplier) in PalaceFont")
+      let font = UIFont(name: TPPConfiguration.ekirjastoFontName(), size: setFontSize) ?? UIFont.systemFont(ofSize: setFontSize, weight: weight)
+      return font
+      // Apply system font size adjustment to the given size
+    } else {
+      print("Using system font size")
       let font = UIFont(name: TPPConfiguration.ekirjastoFontName(), size: UIFont.preferredFont(forTextStyle: textStyle).pointSize) ?? UIFont.systemFont(ofSize: UIFont.preferredFont(forTextStyle: textStyle).pointSize, weight: weight)
-      return UIFontMetrics(forTextStyle: textStyle).scaledFont(for: font)
+      return UIFontMetrics.default.scaledFont(for: font)
+    }
   }
 
   @objc class func semiBoldPalaceFont(for textStyle: UIFont.TextStyle, weight: UIFont.Weight = .regular) -> UIFont {
+    
+    // Retrieve the user-selected font size adjustment (if any)
+    let fontSizeAdjusted = UserDefaults.standard.bool(forKey: "enablePreferences")
+    
+    // Retrieve the multiplier to be applied to given font
+    var fontMultiplier = UserDefaults.standard.double(forKey: "fontMultiplier")
+    if fontMultiplier == 0.0 {
+      fontMultiplier = 1.0
+    }
+    
+    if fontSizeAdjusted {
+      // Apply user font size adjustment to the given font size
+      let baseFontSize = UIFont.preferredFont(forTextStyle: textStyle).pointSize
+      let setFontSize: CGFloat
+      setFontSize = baseFontSize * fontMultiplier
+      print("Adjusted size: \(setFontSize), multiplier: \(fontMultiplier) in semiBoldPalaceFont")
+      let font = UIFont(name: TPPConfiguration.ekirjastoFontName(), size: setFontSize) ?? UIFont.systemFont(ofSize: setFontSize, weight: weight)
+      return font
+      // Apply system font size adjustment to the given size
+    } else {
+      print("Using system font size")
       let font = UIFont(name: TPPConfiguration.ekirjastoFontName(), size: UIFont.preferredFont(forTextStyle: textStyle).pointSize) ?? UIFont.systemFont(ofSize: UIFont.preferredFont(forTextStyle: textStyle).pointSize, weight: weight)
-      return UIFontMetrics(forTextStyle: textStyle).scaledFont(for: font)
+      return UIFontMetrics.default.scaledFont(for: font)
+    }
   }
 
   @objc class func boldPalaceFont(for textStyle: UIFont.TextStyle, weight: UIFont.Weight = .regular) -> UIFont {
+    
+    // Retrieve the user-selected font size adjustment (if any)
+    let fontSizeAdjusted = UserDefaults.standard.bool(forKey: "enablePreferences")
+    
+    // Retrieve the multiplier to be applied to given font
+    var fontMultiplier = UserDefaults.standard.double(forKey: "fontMultiplier")
+    if fontMultiplier == 0.0 {
+      fontMultiplier = 1.0
+    }
+    
+    if fontSizeAdjusted {
+      // Apply user font size adjustment to the given font size
+      let baseFontSize = UIFont.preferredFont(forTextStyle: textStyle).pointSize
+      let setFontSize: CGFloat
+      setFontSize = baseFontSize * fontMultiplier
+      print("Adjusted size: \(setFontSize), multiplier: \(fontMultiplier) in boldPalaceFont")
+      let font = UIFont(name: TPPConfiguration.ekirjastoFontName(), size: setFontSize) ?? UIFont.systemFont(ofSize: setFontSize, weight: weight)
+      return font
+      // Apply system font size adjustment to the given size
+    } else {
+      print("Using system font size")
       let font = UIFont(name: TPPConfiguration.ekirjastoFontName(), size: UIFont.preferredFont(forTextStyle: textStyle).pointSize) ?? UIFont.systemFont(ofSize: UIFont.preferredFont(forTextStyle: textStyle).pointSize, weight: weight)
-      return UIFontMetrics(forTextStyle: textStyle).scaledFont(for: font)
+      return UIFontMetrics.default.scaledFont(for: font)
+    }
   }
-
+  
     /**
-     These three functions below return a dynamically scaled custom font for the specified size.
+     These three functions below return a dynamically scaled custom font for the specified size. If the user has changed the font size in the app, it won't allow system font size changes, and vice versa.
 
      - Parameters:
        - fontSize: The size of the font.
@@ -43,19 +109,88 @@ extension UIFont {
      */
 
   @objc class func palaceFont(ofSize fontSize: CGFloat) -> UIFont {
+    
+    // Retrieve the user-selected font size adjustment (if any)
+    let fontSizeAdjusted = UserDefaults.standard.bool(forKey: "enablePreferences")
+    
+    // Retrieve the multiplier to be applied to given font
+    var fontMultiplier = UserDefaults.standard.double(forKey: "fontMultiplier")
+    if fontMultiplier == 0.0 {
+      fontMultiplier = 1.0
+    }
+
+
+    let setFontSize: CGFloat
+
+    if fontSizeAdjusted {
+      // Apply user font size adjustment to the given font size
+      setFontSize = fontSize * fontMultiplier
+      print("Adjusted size: \(setFontSize), multiplier: \(fontMultiplier) in palaceFont")
+      let font = UIFont(name: TPPConfiguration.ekirjastoFontName(), size: setFontSize) ?? UIFont.systemFont(ofSize: setFontSize)
+      return font
+    // Apply system font size adjustment to the given size
+    } else {
+      print("Using system font size")
       let font = UIFont(name: TPPConfiguration.ekirjastoFontName(), size: fontSize) ?? UIFont.systemFont(ofSize: fontSize)
       return UIFontMetrics.default.scaledFont(for: font)
+    }
   }
 
 
   @objc class func semiBoldPalaceFont(ofSize fontSize: CGFloat) -> UIFont {
+    
+    // Retrieve the user-selected font size adjustment (if any)
+    let fontSizeAdjusted = UserDefaults.standard.bool(forKey: "enablePreferences")
+    
+    // Retrieve the multiplier to be applied to given font
+    var fontMultiplier = UserDefaults.standard.double(forKey: "fontMultiplier")
+    if fontMultiplier == 0.0 {
+      fontMultiplier = 1.0
+    }
+
+
+    let setFontSize: CGFloat
+
+    if fontSizeAdjusted {
+      // Apply user font size adjustment to the given font size
+      setFontSize = fontSize * fontMultiplier
+      print("Adjusted size: \(setFontSize), multiplier: \(fontMultiplier) in semiBoldPalace")
+      let font = UIFont(name: TPPConfiguration.ekirjastoFontName(), size: setFontSize) ?? UIFont.systemFont(ofSize: setFontSize)
+      return font
+    // Apply system font size adjustment to the given size
+    } else {
+      print("Using system font size")
       let font = UIFont(name: TPPConfiguration.ekirjastoFontName(), size: fontSize) ?? UIFont.systemFont(ofSize: fontSize)
       return UIFontMetrics.default.scaledFont(for: font)
+    }
   }
 
 
   @objc class func boldPalaceFont(ofSize fontSize: CGFloat) -> UIFont {
+    
+    // Retrieve the user-selected font size adjustment (if any)
+    let fontSizeAdjusted = UserDefaults.standard.bool(forKey: "enablePreferences")
+    
+    // Retrieve the multiplier to be applied to given font
+    var fontMultiplier = UserDefaults.standard.double(forKey: "fontMultiplier")
+    if fontMultiplier == 0.0 {
+      fontMultiplier = 1.0
+    }
+
+
+    let setFontSize: CGFloat
+
+    if fontSizeAdjusted {
+      // Apply user font size adjustment to the given font size
+      setFontSize = fontSize * fontMultiplier
+      print("Adjusted size: \(setFontSize), multiplier: \(fontMultiplier) in boldPalace")
+      let font = UIFont(name: TPPConfiguration.ekirjastoFontName(), size: setFontSize) ?? UIFont.systemFont(ofSize: setFontSize)
+      return font
+    // Apply system font size adjustment to the given size
+    } else {
+      print("Using system font size")
       let font = UIFont(name: TPPConfiguration.ekirjastoFontName(), size: fontSize) ?? UIFont.systemFont(ofSize: fontSize)
       return UIFontMetrics.default.scaledFont(for: font)
+    }
   }
 }
