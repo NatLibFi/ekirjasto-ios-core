ff667d1749f3bd60abc80d45e557ec77b5da326c
natlibfi-ptalosel <117168919+natlibfi-ptalosel@users.noreply.github.com>
2024-10-17 17:09:44 +0300

    EKIRJASTO-77 Format file

--- Palace/Views/TPPFacetBarView.swift
+++ Palace/Views/TPPFacetBarView.swift
@@ -4,12 +4,12 @@ import Foundation
   func present(_ viewController: UIViewController)
 }
 
-@objcMembers class TPPFacetBarView : UIView {
-  var entryPointView: TPPEntryPointView = TPPEntryPointView()
- 
-  private let borderHeight = 1.0 / UIScreen.main.scale;
-  private let toolbarHeight = CGFloat(40.0);
-
+@objcMembers class TPPFacetBarView: UIView {
+  var entryPointView: TPPEntryPointView = .init()
+  
+  private let borderHeight = 1.0 / UIScreen.main.scale
+  private let toolbarHeight = CGFloat(40.0)
+  
   weak var delegate: TPPFacetBarViewDelegate?
   
   lazy var facetView: TPPFacetView = {
@@ -20,19 +20,20 @@ import Foundation
     
     topBorderView.backgroundColor = UIColor.lightGray.withAlphaComponent(0.9)
     bottomBorderView.backgroundColor = UIColor.lightGray.withAlphaComponent(0.9)
-        
+    
     view.addSubview(bottomBorderView)
     view.addSubview(topBorderView)
-
+    
     bottomBorderView.autoPinEdgesToSuperviewEdges(with: UIEdgeInsets.zero, excludingEdge: .top)
     bottomBorderView.autoSetDimension(.height, toSize: borderHeight)
     topBorderView.autoPinEdgesToSuperviewEdges(with: UIEdgeInsets.zero, excludingEdge: .bottom)
-    topBorderView.autoSetDimension(.height, toSize:borderHeight)
+    topBorderView.autoSetDimension(.height, toSize: borderHeight)
+    
     return view
   }()
   
   @available(*, unavailable)
-  private override init(frame: CGRect) {
+  override private init(frame: CGRect) {
     super.init(frame: frame)
   }
   
@@ -42,8 +43,14 @@ import Foundation
   }
   
   init(origin: CGPoint, width: CGFloat) {
-    
-    super.init(frame: CGRect(x: origin.x, y: origin.y, width: width, height: borderHeight + toolbarHeight + 52.0))
+    super.init(
+      frame: CGRect(
+        x: origin.x,
+        y: origin.y,
+        width: width,
+        height: borderHeight + toolbarHeight + 52.0
+      )
+    )
     
     setupViews()
   }
@@ -51,30 +58,29 @@ import Foundation
   deinit {
     NotificationCenter.default.removeObserver(self)
   }
-
+  
   override func draw(_ rect: CGRect) {
     super.draw(rect)
   }
-
+  
   private func setupViews() {
     backgroundColor = TPPConfiguration.backgroundColor()
-    entryPointView.isHidden = false;
-    facetView.isHidden = true;
-
+    
+    entryPointView.isHidden = false
+    facetView.isHidden = true
+    
     addSubview(facetView)
     addSubview(entryPointView)
-
+    
     setupConstraints()
   }
   
   private func setupConstraints() {
     entryPointView.autoPinEdgesToSuperviewEdges(with: UIEdgeInsets.zero, excludingEdge: .bottom)
-    
     facetView.autoPinEdge(toSuperviewEdge: .leading)
     facetView.autoPinEdge(toSuperviewEdge: .trailing)
     facetView.autoPinEdge(.top, to: .bottom, of: facetView, withOffset: 10.0)
     facetView.autoPinEdge(toSuperviewEdge: .bottom, withInset: 10.0)
-    
     entryPointView.autoPinEdge(.bottom, to: .top, of: facetView)
   }
   
