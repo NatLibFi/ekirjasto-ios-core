b47c444170910d4e51a1b8e566549d1fa7b5a7cf
natlibfi-ptalosel <117168919+natlibfi-ptalosel@users.noreply.github.com>
2024-11-27 16:48:30 +0200

    EKIRJASTO-108 Refactor tab bar and navigation bar related code in TPPAppDelegate.swift

--- Palace/AppInfrastructure/TPPAppDelegate.swift
+++ Palace/AppInfrastructure/TPPAppDelegate.swift
@@ -51,49 +51,111 @@ class TPPAppDelegate: UIResponder, UIApplicationDelegate {
     window?.makeKeyAndVisible()
     window?.rootViewController = TPPRootTabBarController.shared()
     
-    let insets = window?.safeAreaInsets
-    let bottom = insets?.bottom
-    
-    let itemAppearance = UITabBarItemAppearance()
-    itemAppearance.normal.badgePositionAdjustment.horizontal = 3
-    itemAppearance.normal.badgePositionAdjustment.vertical = 1
-    itemAppearance.normal.badgeBackgroundColor = UIColor(named: "ColorEkirjastoRedCircle")
-    itemAppearance.normal.badgeTextAttributes = [.foregroundColor: UIColor(named: "ColorEkirjastoAlwaysBlack")!, .font: UIFont.boldPalaceFont(ofSize: 11)]
-    if let bottom = bottom {
-      itemAppearance.normal.titlePositionAdjustment = UIOffset(horizontal: 5.0, vertical:  CGFloat.minimum(bottom, 6.0))
-    }else {
-      itemAppearance.normal.titlePositionAdjustment = UIOffset(horizontal: 5.0, vertical:  6.0)
-    }
+    let safeAreaInsets = window?.safeAreaInsets
     
     
-    let paragraphStyle = NSMutableParagraphStyle()
-    paragraphStyle.lineBreakMode = .byTruncatingTail
-    paragraphStyle.allowsDefaultTighteningForTruncation = false
+    // UITabBarItemAppearance
+    // Customize the appearance of one tab bar item in different states.
+    // The tab bar item contains icon image, title text and possible badge
+    // States used:
+    //   normal   = inactive tab bar item
+    //   selected = active tab bar item
+    // The appearance we define for the normal state is used as default appearance for tab bar items
     
-    // iPads have more space so set the font to be larger than in iPhones
-    let fontSize = UIDevice.current.userInterfaceIdiom == .pad ? 18.0 : 12.0
-    let font = UIFontMetrics.default.scaledFont(for: UIFont(name: TPPConfiguration.ekirjastoFontName(), size: fontSize) ?? UIFont.systemFont(ofSize: fontSize))
-    let foregroundColor = TPPConfiguration.compatiblePrimaryColor()
+    let tabBarItemAppearance = UITabBarItemAppearance()
     
-    itemAppearance.normal.titleTextAttributes = [
-      .foregroundColor: foregroundColor,
-      .font: font,
-      .paragraphStyle: paragraphStyle
+    // Set badge position
+    // The badge (if appears) is shown in the top right corner of tab bar item
+    tabBarItemAppearance.normal.badgePositionAdjustment = UIOffset(
+      horizontal: 3.0,
+      vertical: 1.0
+    )
+    
+    // Set badge style
+    let badgeBackgroundColor = UIColor(named: "ColorEkirjastoRedCircle")
+    let badgeForegroundColor = UIColor(named: "ColorEkirjastoAlwaysBlack")!
+    let badgeFont = UIFont.boldPalaceFont(ofSize: 11)
+    
+    tabBarItemAppearance.normal.badgeBackgroundColor = badgeBackgroundColor
+    tabBarItemAppearance.normal.badgeTextAttributes = [
+      .foregroundColor: badgeForegroundColor,
+      .font: badgeFont
     ]
     
-    let appearance = UITabBarAppearance()
-    appearance.stackedLayoutAppearance = itemAppearance
-    appearance.inlineLayoutAppearance = itemAppearance
-    appearance.compactInlineLayoutAppearance = itemAppearance
+    // Set title position
+    // Title is shown below the icon in iPhones and on the right side of icon in iPads.
+    // Title is positioned with 2 values:
+    //    horizontal = positive value moves title to the right, we have set this always to be 5.0
+    //    vertical   = positive value moves title downwards, we base this on how much not-used space in bottom we have.
+    //    For example in smaller iPhones there is no extra space at bottom (bottomInset is 0.0)
+    // iPads have more space so we can set the title font to be even larger than in iPhones.
+    let horizontalOffset: CGFloat = 5.0
+    
+    let verticalOffset: CGFloat
+    if let bottomInset = safeAreaInsets?.bottom {
+      verticalOffset = CGFloat.minimum(bottomInset, 6.0)
+    } else {
+      verticalOffset = 6.0
+    }
 
-    UITabBar.appearance().standardAppearance = appearance
-    UITabBar.appearance().tintColor = TPPConfiguration.compatiblePrimaryColor() //Edited by Ebblis
+    tabBarItemAppearance.normal.titlePositionAdjustment = UIOffset(
+      horizontal: horizontalOffset,
+      vertical: verticalOffset
+    )
+    
+    // Set title text style
+    let titleFontSize = UIDevice.current.userInterfaceIdiom == .pad ? 18.0 : 12.0
+    let ekirjastoFont = UIFont(name: TPPConfiguration.ekirjastoFontName(), size: titleFontSize)
+    let systemFont = UIFont.systemFont(ofSize: titleFontSize)
+
+    let titleFont = UIFontMetrics.default.scaledFont(for: ekirjastoFont ?? systemFont)
+    
+    let titleForegroundColor = TPPConfiguration.compatiblePrimaryColor()
+    
+    let titleParagraphStyle = NSMutableParagraphStyle()
+    titleParagraphStyle.lineBreakMode = .byTruncatingTail
+    titleParagraphStyle.allowsDefaultTighteningForTruncation = false
+    
+    tabBarItemAppearance.normal.titleTextAttributes = [
+      .font: titleFont,
+      .foregroundColor: titleForegroundColor,
+      .paragraphStyle: titleParagraphStyle
+    ]
+    
+    
+    // UITabBarAppearance
+    // Set the appearance of the tab bar items in tab bar.
+    // Use the tabBarItemAppearance we already defined for the one tab bar item
+    
+    let tabBarAppearance = UITabBarAppearance()
+    tabBarAppearance.stackedLayoutAppearance = tabBarItemAppearance
+    tabBarAppearance.inlineLayoutAppearance = tabBarItemAppearance
+    tabBarAppearance.compactInlineLayoutAppearance = tabBarItemAppearance
+    
+    
+    // UITabBar
+    // Set the appearance of the tab bar (the bottom bar in app)
+    // Use the tabBarAppearance already defined for the tab bar
+    
+    UITabBar.appearance().standardAppearance = tabBarAppearance
+    UITabBar.appearance().tintColor = TPPConfiguration.compatiblePrimaryColor()
     UITabBar.appearance().backgroundColor = TPPConfiguration.backgroundColor()
+    
+    
+    // UINavigationBar
+    // Set the appearance of the navigation bar (the top bar in app)
+    // Two size variations exist:
+    //      standardAppearance = used if navigation bar is currently of normal (standard) height
+    //      compactAppearance = used if small navigation bar is shown (iPhone in landscape)
+    // Standard and compact apperances are used when user is scrolling down the content in view,
+    // otherwise the scroll edge apperances are used (for example if there is no scrollable content in view)
+    //      scrollEdgeAppearance = used when view is scrolled to top (content top is "touching" the navigation bar)
     
     UINavigationBar.appearance().tintColor = TPPConfiguration.iconColor()
     UINavigationBar.appearance().standardAppearance = TPPConfiguration.defaultAppearance()
     UINavigationBar.appearance().compactAppearance = TPPConfiguration.defaultAppearance()
     UINavigationBar.appearance().scrollEdgeAppearance = TPPConfiguration.defaultAppearance()
+    
     if #available(iOS 15.0, *) {
       UINavigationBar.appearance().compactScrollEdgeAppearance = TPPConfiguration.defaultAppearance()
     }
