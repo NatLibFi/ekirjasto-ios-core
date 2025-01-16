72f30811825c7d2e2d8feebf27e44b12d19ba9c8
natlibfi-ptalosel <117168919+natlibfi-ptalosel@users.noreply.github.com>
2025-01-16 11:23:03 +0200

    EKIRJASTO-117 Update remote HTML resource URLs

--- Palace/Settings/TPPSettings.swift
+++ Palace/Settings/TPPSettings.swift
@@ -176,20 +176,58 @@ func feedbackURL(appLanguage: String!) -> String {
   //    - returns the short alphabetical language code as a string
   //    - current possible E-kirjasto app language codes: fi, sv, en
   static let appLanguage = Locale.current.languageCode
-  
+
   // The URL origin shared with remote NatLibFi HTML pages accessed through application
-  //    - the application language code is part of the URL string ('fi' is set as default)
   //    - with the application language code the user is directed to the corresponding language version of the web page
-  //    - example URL origin: 'https://www.kansalliskirjasto.fi/sv/e-kirjasto'
-  static let ekirjastoURLOrigin = "https://www.kansalliskirjasto.fi/\(appLanguage ?? "fi")/e-kirjasto"
+  //    - example URL origin: 'https://www.kansalliskirjasto.fi/sv/e-biblioteket'
 
-  static let TPPAboutPalaceURLString = "http://thepalaceproject.org/"
-  static let TPPUserAgreementURLString = "\(ekirjastoURLOrigin)/e-kirjaston-kayttoehdot"
-  static let TPPPrivacyPolicyURLString = "\(ekirjastoURLOrigin)/e-kirjaston-tietosuoja-ja-rekisteriseloste"
+  // The URL base is same for all NatLibFi HTML pages
+  static let natlibfiOrigin = "https://www.kansalliskirjasto.fi"
+
+  // E-library HTML pages use different path for each language version
+  // Finnish language version is used as default
+  static let elibraryPath = switch appLanguage {
+  case "fi": "fi/e-kirjasto"
+  case "sv": "sv/e-biblioteket"
+  case "en": "en/e-library"
+  default: "fi/e-kirjasto"
+  }
+
+  static let accessibilityResource = switch appLanguage {
+    case "fi": "e-kirjaston-saavutettavuusseloste"
+    case "sv": "e-bibliotekets-tillganglighetsutlatande"
+    case "en": "e-library-accessibility-statement"
+    default: "e-kirjaston-saavutettavuusseloste"
+  }
+
+  static let instructionsResource = switch appLanguage {
+    case "fi": "e-kirjasto-sovelluksen-kayttoohje"
+    case "sv": "anvisningar-e-biblioteket"
+    case "en": "e-library-instructions"
+    default: "e-kirjasto-sovelluksen-kayttoohje"
+  }
+
+  static let privacyPolicyResource = switch appLanguage {
+    case "fi": "e-kirjaston-tietosuoja-ja-rekisteriseloste"
+    case "sv": "dataskydds-och-registerbeskrivning"
+    case "en": "privacy-policy-data-protection-statement-and-description-data-file"
+    default: "e-kirjaston-tietosuoja-ja-rekisteriseloste"
+  }
+
+  static let userAgreementResource = switch appLanguage {
+  case "fi": "e-kirjaston-kayttoehdot"
+  case "sv": "e-bibliotekets-anvandarvillkor"
+  case "en": "e-library-terms-use"
+  default: "e-kirjaston-kayttoehdot"
+  }
+
+  static let TPPAccessibilityURLString = "\(natlibfiOrigin)/\(elibraryPath)/\(accessibilityResource)"
   static let TPPFeedbackURLString = feedbackURL(appLanguage: appLanguage)
-  static let TPPAccessibilityURLString = "\(ekirjastoURLOrigin)/e-kirjaston-saavutettavuusseloste"
+  static let TPPInstructionsURLString = "\(natlibfiOrigin)/\(elibraryPath)/\(instructionsResource)"
+  static let TPPPrivacyPolicyURLString = "\(natlibfiOrigin)/\(elibraryPath)/\(privacyPolicyResource)"
+  static let TPPUserAgreementURLString = "\(natlibfiOrigin)/\(elibraryPath)/\(userAgreementResource)"
 
-  static private let customMainFeedURLKey = "NYPLSettingsCustomMainFeedURL"
+  private static let customMainFeedURLKey = "NYPLSettingsCustomMainFeedURL"
   static private let accountMainFeedURLKey = "NYPLSettingsAccountMainFeedURL"
   static private let userPresentedAgeCheckKey = "NYPLUserPresentedAgeCheckKey"
   static let userHasAcceptedEULAKey = "NYPLSettingsUserAcceptedEULA"
