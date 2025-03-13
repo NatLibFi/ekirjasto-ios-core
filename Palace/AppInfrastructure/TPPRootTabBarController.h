1f94584769592782f1200701919955b1b649f51d
natlibfi-ptalosel <117168919+natlibfi-ptalosel@users.noreply.github.com>
2025-03-13 17:55:35 +0200

    EKIRJASTO-282 Add new tab for favorites (and other book lists)

--- Palace/AppInfrastructure/TPPRootTabBarController.h
+++ Palace/AppInfrastructure/TPPRootTabBarController.h
@@ -13,6 +13,7 @@
 @property (readonly) TPPR2Owner *r2Owner;
 @property(nonatomic, readonly) TPPCatalogNavigationController *catalogNavigationController;
 @property (nonatomic, readonly) UIViewController *loansAndHoldsViewController;
+@property (nonatomic, readonly) UIViewController *favoritesAndReadViewController;
 @property (nonatomic, readonly) UIViewController *settingsViewController;
 
 - (void)showAndReloadCatalogViewController;
