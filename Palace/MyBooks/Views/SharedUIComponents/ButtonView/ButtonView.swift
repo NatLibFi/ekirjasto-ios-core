c19639f829840f964dae2430ea774cc202ef9d17
natlibfi-ptalosel <117168919+natlibfi-ptalosel@users.noreply.github.com>
2025-04-16 20:22:43 +0300

    EKIRJASTO-284 Add favorite button to MyBooks views

--- Palace/MyBooks/Views/SharedUIComponents/ButtonView/ButtonView.swift
+++ Palace/MyBooks/Views/SharedUIComponents/ButtonView/ButtonView.swift
@@ -9,22 +9,23 @@
 import SwiftUI
 
 struct ButtonView: View {
-  
+
   var title: String
   var indicatorDate: Date? = nil
   var backgroundFill: Color? = nil
   var action: () -> Void
-  
+
   private var accessiblityString: String {
-    guard let untilDate = indicatorDate?.timeUntilString(suffixType: .long) else {
+    guard let untilDate = indicatorDate?.timeUntilString(suffixType: .long)
+    else {
       return title
     }
 
     return "\(title).\(untilDate) remaining"
   }
-    
+
   var body: some View {
-    Button (action: action) {
+    Button(action: action) {
       HStack(alignment: .center, spacing: 5) {
         //indicatorView
         Text(title)
@@ -33,11 +34,18 @@ struct ButtonView: View {
       .frame(minWidth: 0, maxWidth: .infinity)
     }
     .font(Font(uiFont: UIFont.palaceFont(ofSize: 14)))
-    .foregroundColor(indicatorDate != nil ? Color("ColorEkirjastoButtonTextWithBackground") : Color("ColorEkirjastoLabel"))
-    .buttonStyle(AnimatedButton(backgroundColor: indicatorDate != nil ? Color("ColorEkirjastoLightestGreen") : backgroundFill))
+    .foregroundColor(indicatorDate != nil
+        ? Color("ColorEkirjastoButtonTextWithBackground")
+        : Color("ColorEkirjastoLabel")
+    )
+    .buttonStyle(
+      AnimatedButton(backgroundColor: indicatorDate != nil
+        ? Color("ColorEkirjastoLightestGreen")
+        : backgroundFill)
+    )
     .accessibilityLabel(accessiblityString)
   }
-  
+
   /*
    This view is not currently used in My books views,
    as loan time is shown separately and
@@ -58,6 +66,55 @@ struct ButtonView: View {
   }
 }
 
+struct IconButtonView: View {
+
+  var buttonImageForSelectedBook: Image = ImageProviders.MyBooksView.selectionIconCheck
+  var buttonImageForUnselectedBook: Image = ImageProviders.MyBooksView.selectionIconPlus
+  var title: String
+  var indicatorDate: Date? = nil
+  var backgroundFill: Color? = nil
+  var action: () -> Void
+
+  private var accessiblityString: String {
+    "Add or remove book from favorites"
+  }
+
+  var body: some View {
+    Button(action: action) {
+
+      HStack(alignment: .center) {
+        if title == "Select" {
+          buttonImageForUnselectedBook
+            .resizable()
+            .square(length: 25)
+            .foregroundColor(Color("ColorEkirjastoButtonTextWithBackground"))
+        } else if title == "Unselect" {
+          buttonImageForSelectedBook
+            .resizable()
+            .square(length: 25)
+            .foregroundColor(Color("ColorEkirjastoButtonTextWithBackground"))
+        } else {
+          EmptyView()
+        }
+      }
+    }
+    .font(Font(uiFont: UIFont.palaceFont(ofSize: 14)))
+    .fixedSize()
+    .foregroundColor(
+      indicatorDate != nil
+        ? Color("ColorEkirjastoButtonTextWithBackground")
+        : Color("ColorEkirjastoLabel")
+    )
+    .buttonStyle(
+      AnimatedButtonWithoutOverlay(
+        backgroundColor: indicatorDate != nil
+          ? Color("ColorEkirjastoLightestGreen")
+          : backgroundFill)
+    )
+    .accessibilityLabel(accessiblityString)
+  }
+}
+
 struct AnimatedButton: ButtonStyle {
   var backgroundColor: Color? = nil
 
@@ -70,6 +127,18 @@ struct AnimatedButton: ButtonStyle {
         RoundedRectangle(cornerRadius: 3)
           .stroke(Color("ColorEkirjastoGreen"), lineWidth: 1)
       )
+      .opacity(configuration.isPressed ? 0.5 : 1.0)
+  }
+}
+
+struct AnimatedButtonWithoutOverlay: ButtonStyle {
+  var backgroundColor: Color? = nil
+
+  func makeBody(configuration: Configuration) -> some View {
+    configuration.label
+      .frame(height: 35)
+      .buttonStyle(.plain)
+      .background(backgroundColor)
       .opacity(configuration.isPressed ? 0.5 : 1.0)
   }
 }
