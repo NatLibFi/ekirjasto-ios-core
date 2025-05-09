8dd1745dd33d77e9c6bc901570fd95d3113a42dc
natlibfi-ptalosel <117168919+natlibfi-ptalosel@users.noreply.github.com>
2025-05-09 15:37:48 +0300

    EKIRJASTO-285-286 Create drafts for book select and unselect functions

--- Palace/MyBooks/Actions/Selecting/MyBooksSelectionCenter.swift
+++ Palace/MyBooks/Actions/Selecting/MyBooksSelectionCenter.swift
@@ -22,4 +22,56 @@ class MyBooksSelectionCenter: NSObject {
     super.init()
   }
 
+  // MARK: - Select book functions: adding book to favorites
+  @objc func startBookSelect(
+    for book: TPPBook,
+    completion: (() -> Void)? = nil
+  ) {
+
+    let selectionState: BookSelectionState = bookRegistry.selectionState(
+      for: book.identifier)
+    let selectionStateAsString: String = BookSelectionStateHelper.stringValue(
+      from: selectionState)
+
+    ATLog(
+      .info,
+      "Start book select for book "
+        + "with title: '\(book.title)' "
+        + "and selection state: \(selectionStateAsString)"
+    )
+
+    bookRegistry.setProcessing(true, for: book.identifier)
+
+    // check if user is logged in in app
+    // update book registry with selected book record
+    // send select request to backend
+    // show notification to user
+  }
+
+  // MARK: - Unselect book functions: removing book from favorites
+  @objc func startBookUnselect(
+    for book: TPPBook,
+    completion: (() -> Void)? = nil
+  ) {
+
+    let selectionState: BookSelectionState = bookRegistry.selectionState(
+      for: book.identifier)
+    let selectionStateAsString: String = BookSelectionStateHelper.stringValue(
+      from: selectionState)
+
+    ATLog(
+      .info,
+      "Start book unselect for book "
+        + "with title: '\(book.title)' "
+        + "and selection state: \(selectionStateAsString)"
+    )
+
+    bookRegistry.setProcessing(true, for: book.identifier)
+
+    // check if user is logged in in app
+    // update book registry with unselected book record
+    // send unselect request to backend
+    // show notification to user
+  }
+
 }
