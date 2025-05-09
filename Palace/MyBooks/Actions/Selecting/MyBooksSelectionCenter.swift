32826f44ec135425b19a925fba109782c2e6cb27
natlibfi-ptalosel <117168919+natlibfi-ptalosel@users.noreply.github.com>
2025-05-09 16:07:20 +0300

    EKIRJASTO-285-286 Check that user is logged in before selection action

--- Palace/MyBooks/Actions/Selecting/MyBooksSelectionCenter.swift
+++ Palace/MyBooks/Actions/Selecting/MyBooksSelectionCenter.swift
@@ -23,10 +23,17 @@ class MyBooksSelectionCenter: NSObject {
   }
 
   // MARK: - Select book functions: adding book to favorites
+
   @objc func startBookSelect(
     for book: TPPBook,
     completion: (() -> Void)? = nil
   ) {
+
+    if !bookHasIdentifier(book) {
+      // if book has no identifier
+      // do not proceed with selecting
+      return
+    }
 
     let selectionState: BookSelectionState = bookRegistry.selectionState(
       for: book.identifier)
@@ -40,19 +47,44 @@ class MyBooksSelectionCenter: NSObject {
         + "and selection state: \(selectionStateAsString)"
     )
 
-    bookRegistry.setProcessing(true, for: book.identifier)
+    startProcessing(book)
 
-    // check if user is logged in in app
-    // update book registry with selected book record
-    // send select request to backend
-    // show notification to user
+    if isLoginRequired() == true {
+      // user is not logged in
+      // stop book processing and
+      // show the login view for user
+      stopProcessing(book)
+      EkirjastoLoginViewController.show {}
+    } else {
+      // user is logged in
+      // proceed with selecting book
+      sendSelectBookRequestAndUpdateBookRegistry(
+        book: book,
+        completion: completion
+      )
+    }
+  }
+
+  private func sendSelectBookRequestAndUpdateBookRegistry(
+    book: TPPBook,
+    completion: (() -> Void)? = nil
+  ) {
+    // send select book request
+    // if success, update book registry
   }
 
   // MARK: - Unselect book functions: removing book from favorites
+
   @objc func startBookUnselect(
     for book: TPPBook,
     completion: (() -> Void)? = nil
   ) {
+
+    if bookHasIdentifier(book) == false {
+      // if book has no identifier
+      // do not proceed with unselecting
+      return
+    }
 
     let selectionState: BookSelectionState = bookRegistry.selectionState(
       for: book.identifier)
@@ -66,12 +98,60 @@ class MyBooksSelectionCenter: NSObject {
         + "and selection state: \(selectionStateAsString)"
     )
 
-    bookRegistry.setProcessing(true, for: book.identifier)
+    startProcessing(book)
 
-    // check if user is logged in in app
-    // update book registry with unselected book record
-    // send unselect request to backend
-    // show notification to user
+    if isLoginRequired() == true {
+      // user is not logged in
+      // stop book processing and
+      // show the login view for user
+      stopProcessing(book)
+      EkirjastoLoginViewController.show {}
+    } else {
+      // user is logged in
+      // proceed with unselecting book
+      sendUnselectBookRequestAndUpdateBookRegistry(
+        book: book,
+        completion: completion
+      )
+    }
+
+  }
+
+  func sendUnselectBookRequestAndUpdateBookRegistry(
+    book: TPPBook,
+    completion: (() -> Void)? = nil
+  ) {
+    // send unselect book request
+    // if success, update book registry
+  }
+
+  // MARK: - Helper functions for book selection functions
+
+  private func bookHasIdentifier(_ book: TPPBook) -> Bool {
+    if book.identifier.isEmpty {
+      return false
+    }
+
+    return true
+  }
+
+  private func startProcessing(_ book: TPPBook) {
+    bookRegistry.setProcessing(true, for: book.identifier)
+  }
+
+  private func stopProcessing(_ book: TPPBook) {
+    bookRegistry.setProcessing(false, for: book.identifier)
+  }
+
+  private func isLoginRequired() -> Bool {
+    let needsAuthentication = userAccount.authDefinition?.needsAuth ?? true
+    let isAuthenticated = userAccount.hasCredentials()
+
+    if !isAuthenticated || needsAuthentication {
+      return true
+    }
+
+    return false
   }
 
 }
