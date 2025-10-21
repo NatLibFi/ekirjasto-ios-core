2802af3bff6420f9c9bfd4cdd71a9c8cc0b8a400
natlibfi-ptalosel <117168919+natlibfi-ptalosel@users.noreply.github.com>
2025-10-21 17:01:48 +0300

    EKIRJASTO-548 Add drafts for toast service and view

---
+++ Palace/Toast/ToastService.swift
@@ -0,0 +1,22 @@
+//
+//  ToastService.swift
+//
+
+import SwiftUI
+
+
+// MARK: - Service for showing toast messages
+
+// Class for displaying toast messages on the screen
+class ToastService {
+
+  
+  // MARK: - Initialize ToastService
+
+  private init() {}
+
+  
+  // func showToastMessage
+  
+
+}
---
+++ Palace/Toast/ToastView.swift
@@ -0,0 +1,17 @@
+//
+//  ToastView.swift
+//
+
+import SwiftUI
+
+
+// MARK: - Toast message SwiftUI view
+
+struct ToastView: View {
+
+  var body: some View {
+    Text("ToastMessage")
+    // Android style
+  }
+
+}
